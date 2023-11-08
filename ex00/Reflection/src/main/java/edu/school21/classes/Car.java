44bf3d503d54502939456d3aed79727a1ace4b56
Arthur <81766213+ArthurJann@users.noreply.github.com>
2023-11-08 20:12:01 +0300

    Add files via upload

---
+++ ex00/Reflection/src/main/java/edu/school21/classes/Car.java
@@ -0,0 +1,12 @@
+package edu.school21.classes;
+
+public class Car {
+    public int use() {
+        return 5;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
